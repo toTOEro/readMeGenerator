017fe3314bacb68d4168751a19adfd753fe54df9
anthony3738-miZJgd <anthony3738@gmail.com>
2022-08-10 21:16:20 -0700

    Main functionality implemented for generation of readMe

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,126 @@
-// TODO: Include packages needed for this application
+// This code requires the inquirer and FS packages in order to properly funcion.
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+const questions = [
+    {
+        type: 'input',
+        message: 'Provide a file name:',
+        name: 'fileName',
+        validate: input => input ? true : console.warn('\nPlease provide a value'),
+        default: 'README',
+    },
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+        validate: input => input ? true : console.warn('\nPlease provide a value'),
+    },
+    {
+        type: 'input',
+        message: 'Describe your project: What is the motivation? What does it solve?',
+        name: 'description',
+        validate: input => input ? true : console.warn('\nPlease provide a value'),
+    },
+    {
+        type: 'input',
+        message: 'How do you install the project?',
+        name: 'installation',
+        validate: input => input ? true : console.warn('\nPlease provide a value'),
+
+    },
+    {
+        type: 'input',
+        message: 'How do you use the project?',
+        name: 'usage',
+        validate: input => input ? true : console.warn('\nPlease provide a value'),
+
+    },
+    {
+        type: 'input',
+        message: 'Who contributed? List in this format: username2, username2, ... ',
+        name: 'credits',
+        validate: input => input ? true : console.warn('\nPlease provide a value'),
+
+    },
+    {
+        type: 'list',
+        message: 'License?',
+        name: 'license',
+        choices: [
+            'None',
+            'Apache',
+            'Boost',
+            'BSD',
+            'Creative Commons',
+            'Eclipse',
+            'GNU GPLv3',
+            'Hippocratic',
+            'IBM',
+            'ISC',
+            'MIT',
+            'Mozilla',
+            'Open Data Commons',
+            'PERL',
+            'SIL',
+            'Unlicense',
+            'WTFPL',
+            'Zlib',
+        ],
+    },
+    {
+        type: 'input',
+        message: 'Contribution Guidelines?',
+        name: 'contribution',
+        default: 'None'
+    },
+    {
+        type: 'input',
+        message: 'Test instructions?',
+        name: 'tests',
+        default: 'None'
+    },
+    {
+        type: 'input',
+        message: 'Image path?',
+        name: 'image',
+        default: 'None'
+    },
+    {
+        type: 'input',
+        message: 'Email Address?',
+        name: 'email',
+        validate: input => input ? true : console.warn('\nPlease provide a value'),
+
+    },
+    // {
+    //     type: 'input',
+    //     message: 'Questions?',
+    //     name: 'questions',
+    //     default: ''
+    // },
+];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    fs.writeFile(fileName,data, (err) => 
+        err ? console.error('Error in writing File!') : console.log('ReadMe successfully generated!')
+    );
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((response) =>
+            writeToFile(response.fileName + '.md', generateMarkdown(response))
+        )
+
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,72 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const { title, description, installation, usage, credits, license, badges, contribution, tests } = data;
 
-`;
+  // Handling table of contents if there are no contribution instructions or test instructions provided
+  const contributeTbl = contribution !== 'None' ? `\n  - [How to Contribute](#how-to-contribute)` : '';
+  const testTbl = tests !== 'None' ? `\n  - [Tests](#tests)` : '';
+  const modularTable = contributeTbl + testTbl;
+  const tblOfContents = `
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)   ${modularTable}
+  `;
+
+
+  // Handling the inclusion of contribution instructions or test instructions if none are provided
+  const contributionSect = contribution !== 'None' ? `\n  ## How to Contribute\n\n  ${contribution} \n` : '' ;
+  const testSect = tests !== 'None' ? `\n  ## Tests\n\n  ${tests} \n` : '';
+  const modularSection = contributionSect + testSect;
+
+  // readMe template derived from https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
+  return `
+  # ${title}}
+
+  ## Description
+  ${description}
+
+
+  ## Table of Contents 
+
+  ${tblOfContents}
+
+  ## Installation
+
+  ${installation}
+
+  ## Usage
+
+  ${usage}
+  
+
+  ## Credits
+
+  ${credits} 
+
+  ## License
+
+  ${license}
+
+
+  ## Badges
+
+  ${badges}
+  ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+  ${modularSection}
+  `;
 }
 
 module.exports = generateMarkdown;
